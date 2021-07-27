78a23a763b6d9fb21c158dca0c54a50a0fbb00e0
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-27 20:57:38 +0100

    created landing and about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const aboutPage = () => {
+    return (
+        <main>
+            <title>About Page</title>
+            <h1>Welcome to my Developers CV</h1>
+            <p>This site will be developed using Gatsby!</p>
+        </main>
+    )
+}
+
+export default aboutPage;
